023ac9382e743236a93144cc81ceb93289541ec7
Giorgio Mondauto <giorgiomondauto@Giorgios-MacBook-Pro.local>
2020-10-24 16:07:05 +0100

    data.to_csv(data.csv, header=True, index=False)

--- WebScraping/scrap.py
+++ WebScraping/scrap.py
@@ -12,22 +12,18 @@ sentences = []
 def get_urls():
     # Source to get data 
     BASE_URL = 'https://fangj.github.io/friends/'
-
-
     base_page = requests.get(BASE_URL)
     tree = html.fromstring(base_page.content)
-
-
     # All urls to scrap
     URLS = [ BASE_URL+href for href in tree.xpath('/html/body/ul/li/a/@href')]
-
     return URLS
 
-
-#To parse each element 
-#retain only actor line
-#remove text in parenthesis and bracket
 def parse_content(elmts):
+    '''
+    To parse each element 
+    retain only actor line
+    remove text in parenthesis and bracket
+    '''
     parsed_content = []
     for elmt in elmts:
         content = elmt.text
@@ -41,13 +37,11 @@ def parse_content(elmts):
             key, quote = content.split(':')
             sentences.append((key,quote))
 
-
-
-
-
-# For each url ,get content of the page 
-# get  all <p> elements
 def fetch_pages(urls):
+    '''
+    For each url ,get content of the page 
+    get  all <p> elements
+    ''' 
     failed_urls = []
     N = len(urls)
     i = 0
@@ -68,22 +62,13 @@ def fetch_pages(urls):
             i+= 1
             print(f'page {i} on {N} processed')
 
-    
-
     #Save all the scraped data in csv file
     data = pd.DataFrame(sentences, columns=['actor','quote'])
-    data.to_csv('data.csv',sep='\t', header=True, index=False)
+    data.to_csv('data.csv', header=True, index=False)
 
     #check if failed url 
     if len(failed_urls) > 0:
         print(failed_urls)
 
-    
 urls = get_urls()
 fetch_pages(urls)
-
-
-
-
-
-
